a4fd519c4f702733b8e18ec099a4f9f7946c751d
Alex <engla.alex@protonmail.com>
2026-06-10 15:29:56 +0200

    Removed getServices
    Services that are discovered are stored in CasualInfo in jca instead of using caller

--- casual/casual-caller-api/src/main/java/se/laz/casual/connection/caller/info/CasualInfo.java
+++ casual/casual-caller-api/src/main/java/se/laz/casual/connection/caller/info/CasualInfo.java
@@ -1,9 +1,6 @@
 package se.laz.casual.connection.caller.info;
 
-import java.util.List;
-
 public interface CasualInfo
 {
-    List<Service> getServices();
     void discoverService(String service);
 }
--- casual/casual-caller/src/main/java/se/laz/casual/connection/caller/info/CasualInfoImpl.java
+++ casual/casual-caller/src/main/java/se/laz/casual/connection/caller/info/CasualInfoImpl.java
@@ -1,55 +1,29 @@
+/*
+ * Copyright (c) 2026, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
 package se.laz.casual.connection.caller.info;
 
 import jakarta.ejb.Remote;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
-import se.laz.casual.connection.caller.Cache;
-import se.laz.casual.connection.caller.ConnectionFactoriesByPriority;
 import se.laz.casual.connection.caller.ConnectionFactoryLookup;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
 
 @Remote(CasualInfo.class)
 @Stateless
-public class CasualInfoImpl implements CasualInfo
-{
-    private static final Logger LOG = Logger.getLogger(CasualInfoImpl.class.getName());
+public class CasualInfoImpl implements CasualInfo {
 
-    Cache cache;
     ConnectionFactoryLookup connectionFactoryLookup;
 
     @Inject
-    public CasualInfoImpl(Cache cache, ConnectionFactoryLookup connectionFactoryLookup) {
-        this.cache = cache;
+    public CasualInfoImpl(ConnectionFactoryLookup connectionFactoryLookup) {
         this.connectionFactoryLookup = connectionFactoryLookup;
     }
 
     @Override
-    public List<Service> getServices() {
-        List<Service> services = new ArrayList<>();
-        // Get all cached service names
-        cache.getServices().forEach(service -> {
-            // For a specific service, multiple connections can occur
-            ConnectionFactoriesByPriority connectionFactoriesByPriority = cache.get(service);
-            // Each connection has a priority, which also represents the number of hops to the service
-            connectionFactoriesByPriority.getOrderedKeys().forEach(priority -> {
-                // For each connection, add a service entry
-                connectionFactoriesByPriority.getForPriority(priority).forEach(connectionFactoryEntry ->
-                        services.add(new Service.Builder().name(service)
-                                .hops(priority)
-                                .jndiName(connectionFactoryEntry.getJndiName())
-                                .valid(connectionFactoryEntry.isValid()).build()));
-            });
-
-        });
-        return services;
-    }
-
-    @Override
-    public void discoverService(String serviceName)
-    {
+    public void discoverService(String serviceName) {
         // Trigger discovery on service (save connections to cache)
         connectionFactoryLookup.get(serviceName);
     }
